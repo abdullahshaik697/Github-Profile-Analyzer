14608d0205cfc563e093762b445764d7da437f22
Abdullah Shaikh <abdullahshaik55555@gmail.com>
2026-03-31 01:53:13 +0500

    finished routing problem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Login from "./pages/Login"
 import { ProtectedRoute } from "./ProtectedRoute"
@@ -10,10 +10,8 @@ function App() {
   return (
     <>
       <Routes>
-
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} ></Route>
-
-
         <Route path="/dashboard" element={
           <ProtectedRoute>
             <Dashboard />
